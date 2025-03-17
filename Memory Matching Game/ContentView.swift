860c6fe20deae8258af776cfe3eecb4a35822353
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2025-03-17 12:03:31 -0700

    iterating thru emojis using ForEach view builder

--- Memory Matching Game/ContentView.swift
+++ Memory Matching Game/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🥳", "🦐", "⛳️", "🦎"]
+    
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index], isFaceUp: true)
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,6 +22,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
     
     var body: some View {
@@ -29,7 +31,7 @@ struct CardView: View {
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("🥳")
+                Text(content)
                     .font(.largeTitle)
             } else {
                 base
